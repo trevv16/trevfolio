fd7a104549cca9f7a1ab5b4de3b9ca3531f0030e
Trevor Njeru <tnjeru16@gmail.com>
2020-09-03 18:35:18 -0400

    renamed db_generator, added db_config

--- backend/config/db_config.js
+++ backend/config/db_config.js
@@ -1,49 +1,19 @@
-const fs = require("fs");
-// const dummy = require("mongoose-dummy");
-const dataFolder = "./dummy-data";
-const User = require("../models/user");
-// const {
-//  User,
-//  Project,
-//  Email,
-//  Blog,
-//  Post,
-//  Resume,
-//  Skill,
-//  MailingList,
-//  Gallery,
-// } = require("../models");
-const ignored = ["_id"];
+function initialize(mongoose, dbUser, dbPW, dbName) {
+ mongoose.connect(
+  `mongodb+srv://${process.env.MONOG_ATLAS_USER}:${process.env.MONGO_ATLAS_PW}@dev-rv8ag.mongodb.net/${process.env.MONGO_ATLAS_DB_NAME}?retryWrites=true&w=majority`,
+  {
+   useNewUrlParser: true,
+   useFindAndModify: false,
+   useCreateIndex: true,
+   useUnifiedTopology: true,
+  }
+ );
 
-generateFiles(User, "user");
-// generateFiles(Project, "project");
-// generateFiles(Email, "email");
-// generateFiles(Blog, "blog");
-// generateFiles(Post, "post");
-// generateFiles(Resume, "resume");
-// generateFiles(Skill, "skill");
-// generateFiles(MailingList, "mailingList");
-// generateFiles(Gallery, "gallery");
-
-function generateFiles(Model, fileName) {
- const location = `${dataFolder}/${fileName}.json`;
- const data = JSON.stringify(getModelData(Model));
-
- fs.writeFile(location, data, function (err) {
-  if (err) throw err;
-  console.log("Generated dummy data for", fileName);
+ const db = mongoose.connection;
+ db.on("error", console.error.bind(console, "connection error:"));
+ db.once("open", function () {
+  // we're connected!
  });
 }
 
-function getModelData(Model) {
- return dummy(Model, {
-  ignore: ignored,
-  returnDate: true,
-  applyFilter: true,
-  //   custom: {
-  //    phone: "String",
-  //    email: "String",
-  //    password: "String",
-  //   },
- });
-}
+module.exports = initialize;
