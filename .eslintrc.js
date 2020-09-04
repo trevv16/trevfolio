c4be05f6f2c6579460414ebe256826df5bad4837
Trevor Njeru <tnjeru16@gmail.com>
2020-09-04 12:00:49 -0400

    updated development settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,19 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-  },
-  extends: [
-    "prettier",
-    "airbnb-base",
-    "plugin:promise/recommended",
-    "plugin:jest/recommended",
-  ],
-  plugins: ["promise", "jest"],
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: "module",
-  },
-  rules: {},
+ env: {
+  browser: true,
+  es2020: true,
+ },
+ extends: [
+  "prettier",
+  "airbnb-base",
+  "plugin:react/recommended",
+  "plugin:promise/recommended",
+  "plugin:jest/recommended",
+ ],
+ plugins: ["promise", "jest"],
+ parserOptions: {
+  ecmaVersion: 6,
+  sourceType: "module",
+ },
+ rules: {},
 };
