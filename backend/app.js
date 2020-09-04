6d70d7e67edb974b5fc7bd02e5d3b66f38f94d98
Trevor Njeru <tnjeru16@gmail.com>
2020-09-03 21:44:07 -0400

    server config fixes

--- backend/app.js
+++ backend/app.js
@@ -64,6 +64,10 @@ app.use(methodOverride("_method"));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Set Routes
+app.use("/", indexRouter);
+app.use("/user", usersRouter);
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
  next(createError(404));
