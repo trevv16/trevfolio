d8ab8b518ec054c09ce9120b7b469ecd15bc6fc0
Trevor Njeru <tnjeru16@gmail.com>
2020-09-08 14:14:46 -0400

    updated file path routing

---
+++ src/frontend/src/components/index.js
@@ -0,0 +1,18 @@
+export { default as Introduction } from '../components/_home_page/intro/Introduction';
+export { default as Jumbotron } from '../components/_home_page/jumbotron/Jumbotron';
+export { default as MainSkills } from '../components/_home_page/main_skills/MainSkills';
+export { default as RecentProjects } from '../components/_home_page/recent_projects/RecentProjects';
+
+export { default as CareerStats } from '../components/_resume_page/career_stats/CareerStats';
+export { default as Education } from '../components/_resume_page/education_history/EducationHistory';
+export { default as Github } from '../components/_resume_page/github/Github';
+export { default as EducationHistory } from '../components/_resume_page/education_history/EducationHistory';
+export { default as Header } from '../components/_resume_page/header/Header';
+export { default as WorkHistory } from '../components/_resume_page/work_history/WorkHistory';
+export { default as Skills } from '../components/_resume_page/skills/Skills';
+
+export { default as ContactForm } from '../components/contact_form/ContactForm';
+export { default as Gallery } from '../components/gallery/Gallery';
+export { default as MailingList } from '../components/mailing_list/MailingList';
+export { default as Navigation } from '../components/partials/navigation/Navigation';
+export { default as Footer } from '../components/partials/footer/Footer';
--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from '../app/App';
-import * as serviceWorker from '../../serviceWorker';
+import './index.scss';
+import App from './views/app/App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
