21fa6f242ba16a1de586d0b03b345634d4cc7798
Trevor Njeru <tnjeru16@gmail.com>
2020-09-08 16:00:03 -0400

    created public page templates

---
+++ src/frontend/src/views/resume/Resume.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {} from '@material-ui/core';
+import {} from '@material-ui/icons';
+import api from '../../utils/api';
+import { MailingList, Navigation, Footer } from '../../components/index';
+
+export default class Resume extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <MailingList />
+        <Footer />
+      </div>
+    );
+  }
+}
