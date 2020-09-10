3f7b58bc1734486cc8455943d1f45d6dee46fd33
Trevor Njeru <tnjeru16@gmail.com>
2020-09-10 02:08:34 -0400

    updated front-end configs with app themeing

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -3,10 +3,67 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {
+  createMuiTheme,
+  ThemeProvider,
+  responsiveFontSizes
+} from '@material-ui/core';
+
+let theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#687395',
+      main: '#3c4867',
+      dark: '#11213c'
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      light: '#69ff9a',
+      main: '#23ce6b',
+      dark: '#009c3e'
+    },
+    success: {
+      main: '#229631',
+      dark: '#1a7426'
+    },
+    danger: {
+      main: '#f46036',
+      dark: '#e93c0c'
+    },
+    warning: {
+      main: '#fbbe4b',
+      dark: '#faa80f'
+    },
+    info: {
+      main: '#59a5d8',
+      dark: '#2f8ac6'
+    },
+    // Used by `getContrastText()` to maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 3,
+    // Used by the functions below to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2
+  },
+  typography: {
+    fontFamily: '"Poppins", sans-serif'
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        '@font-face': '"Poppins", sans-serif'
+      }
+    }
+  }
+});
+theme = responsiveFontSizes(theme);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
