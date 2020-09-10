9a93f0e7dfc4e3eb61bde3b73f4cc564c47bade5
Trevor Njeru <tnjeru16@gmail.com>
2020-09-10 00:24:28 -0400

    created footer component

--- src/frontend/src/components/partials/footer/Footer.js
+++ src/frontend/src/components/partials/footer/Footer.js
@@ -1,6 +1,44 @@
 import React, { Component } from 'react';
+import {
+  CssBaseline,
+  Container,
+  Link,
+  Typography,
+  withStyles
+} from '@material-ui/core';
 
-export default class Footer extends Component {
+function Copyright() {
+  return (
+    <React.Fragment>
+      <Typography variant='body2' color='textSecondary'>
+        {'Copyright © '}
+        <Link color='inherit' href='https://trevornjeru.com/'>
+          Trevor Njeru
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    </React.Fragment>
+  );
+}
+
+const useStyles = (theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh'
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[200]
+        : theme.palette.grey[800]
+  }
+});
+
+class Footer extends Component {
   constructor(props) {
     super(props);
 
@@ -8,6 +46,19 @@ export default class Footer extends Component {
   }
 
   render() {
-    return <div>Footer</div>;
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <footer className={classes.footer}>
+          <Container maxWidth='sm'>
+            <Copyright />
+          </Container>
+        </footer>
+      </div>
+    );
   }
 }
+
+export default withStyles(useStyles)(Footer);
