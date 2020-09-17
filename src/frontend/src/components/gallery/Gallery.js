4e5084c4f54b6e1da99dc03036ef9f1514949794
Trevor Njeru <tnjeru16@gmail.com>
2020-09-17 17:05:39 -0400

    add react-material-ui pkg and created gallery comp

--- src/frontend/src/components/gallery/Gallery.js
+++ src/frontend/src/components/gallery/Gallery.js
@@ -1,9 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { CssBaseline, makeStyles } from '@material-ui/core';
+import Carousel from 'react-material-ui-carousel';
 
-export default class Gallery extends Component {
-  constructor(props) {
-    super(props);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginTop: theme.spacing(10),
+    marginBottom: theme.spacing(10)
+  },
+  carousel: {}
+}));
 
-    this.state = {};
-  }
+function Gallery() {
+  const classes = useStyles();
+  const images = [
+    {
+      name: 'image1',
+      src: 'https://source.unsplash.com/random/1080x1080',
+      description: 'Picture of me'
+    },
+    {
+      name: 'image2',
+      src: 'https://source.unsplash.com/random/1080x1080',
+      description: 'Picture of me'
+    },
+    {
+      name: 'image2',
+      src: 'https://source.unsplash.com/random/1080x1080',
+      description: 'Picture of me'
+    }
+  ];
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Carousel
+        navButtonsAlwaysVisible
+        animation='slide'
+        className={classes.carousel}
+      >
+        {images.map((src, description, i) => (
+          <img key={i} src={src} alt={description} />
+        ))}
+      </Carousel>
+    </div>
+  );
 }
+
+export default Gallery;
