314e34300b1251eced4015bccc03ce58f52f8599
Trevor Njeru <tnjeru16@gmail.com>
2020-09-17 17:22:10 -0400

    updated jumbotron with carousel support

--- src/frontend/src/components/_home_page/jumbotron/Jumbotron.js
+++ src/frontend/src/components/_home_page/jumbotron/Jumbotron.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { CssBaseline, makeStyles, Grid } from '@material-ui/core';
+import Carousel from 'react-material-ui-carousel';
 
 const useStyles = makeStyles((theme) => ({
   jumboBox: {
     marginTop: theme.spacing(3),
     overflow: 'hidden'
   },
-  jumboMedia: {
+  carousel: {
+    alignItems: 'center',
+    justifyContent: 'center',
     maxWidth: '100%',
     maxHeight: '100%'
   }
@@ -14,17 +17,40 @@ const useStyles = makeStyles((theme) => ({
 
 function Jumbotron() {
   const classes = useStyles();
+  const images = [
+    {
+      name: 'image1',
+      src: 'https://source.unsplash.com/random/1920x600',
+      description: 'Trev'
+    },
+    {
+      name: 'image2',
+      src: 'https://source.unsplash.com/random/1920x600',
+      description: 'Trev'
+    },
+    {
+      name: 'image2',
+      src: 'https://source.unsplash.com/random/1920x600',
+      description: 'Trev'
+    }
+  ];
 
   return (
     <div>
       <CssBaseline />
       <Grid container xs={12} className={classes.jumboBox}>
         <Grid item xs={12}>
-          <img
-            src='https://source.unsplash.com/random/1920x500'
-            alt='jumbotron'
-            className={classes.jumboMedia}
-          />
+          <Carousel
+            navButtonsAlwaysVisible
+            animation='slide'
+            interval={5500}
+            timeout={800}
+            className={classes.carousel}
+          >
+            {images.map((img, i) => (
+              <img key={i} src={img.src} alt={img.description} />
+            ))}
+          </Carousel>
         </Grid>
       </Grid>
     </div>
