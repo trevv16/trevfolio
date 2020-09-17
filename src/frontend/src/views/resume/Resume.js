f070aa5efeb34ed6d8fe534ae3f5724a57db317c
Trevor Njeru <tnjeru16@gmail.com>
2020-09-17 17:04:26 -0400

    updated resume component details.

--- src/frontend/src/views/resume/Resume.js
+++ src/frontend/src/views/resume/Resume.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
-import {} from '@material-ui/core';
-import {} from '@material-ui/icons';
-import api from '../../utils/api';
+
+import {
+  CssBaseline,
+  Link,
+  Grid,
+  Typography,
+  makeStyles
+} from '@material-ui/core';
+// import {} from '@material-ui/icons';
+// import api from '../../utils/api';
 import { MailingList, Navigation, Footer } from '../../components/index';
 
 export default class Resume extends Component {
@@ -11,10 +18,73 @@ export default class Resume extends Component {
     this.state = {};
   }
 
+  props = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1
+    },
+    main: {
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: theme.spacing(18),
+      marginBottom: theme.spacing(2)
+    },
+    form: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      margin: theme.spacing(8, 0)
+    },
+    messageBox: {
+      // padding: theme.spacing(8),
+      margin: theme.spacing(18)
+    },
+    messageDesc: {
+      marginTop: theme.spacing(3)
+    },
+    nameField: {
+      width: '100%'
+    },
+    emailField: {
+      width: '100%'
+    },
+    subButton: {
+      justifyContent: 'end',
+      alignItems: 'center',
+      height: '6vh'
+    }
+  }));
+
   render() {
+    const classes = this.props;
     return (
-      <div>
+      <div className={classes.root}>
+        <CssBaseline />
         <Navigation />
+
+        <Grid container xs={12} spacing={1} className={classes.main}>
+          <Grid item xs={10}>
+            <Typography align='center' variant='h1' component='h1'>
+              Resume
+            </Typography>
+          </Grid>
+          <Grid item xs={10}>
+            <Typography
+              align='center'
+              variant='h6'
+              component='h6'
+              className={classes.messageDesc}
+            >
+              If you have a question or would like to get in contact with me,
+              reach out to me{' '}
+              <Link href='/contact' color='secondary'>
+                here
+              </Link>
+              .
+            </Typography>
+          </Grid>
+        </Grid>
+
         <MailingList />
         <Footer />
       </div>
