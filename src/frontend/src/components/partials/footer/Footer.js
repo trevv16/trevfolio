34969a0682e73a14ff942c207a9b1f7d97c4a1a1
Trevor Njeru <tnjeru16@gmail.com>
2020-09-19 17:37:17 -0400

    added social icons to footer

--- src/frontend/src/components/partials/footer/Footer.js
+++ src/frontend/src/components/partials/footer/Footer.js
@@ -1,5 +1,37 @@
 import React from 'react';
-import { CssBaseline, Link, Typography, makeStyles } from '@material-ui/core';
+import {
+  CssBaseline,
+  Link,
+  Typography,
+  makeStyles,
+  Grid
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '6vh',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    marginTop: 'auto',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[200]
+        : theme.palette.grey[800]
+  },
+  icon_group: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '2vh'
+  },
+  social_icons: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '2vw',
+    height: 'auto',
+    opacity: '.4'
+  }
+}));
 
 function Copyright() {
   return (
@@ -14,21 +46,44 @@ function Copyright() {
   );
 }
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '6vh',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: theme.spacing(3, 2),
-    marginTop: 'auto',
-    backgroundColor:
-      theme.palette.type === 'light'
-        ? theme.palette.grey[200]
-        : theme.palette.grey[800]
-  }
-}));
+function Icons() {
+  const classes = useStyles();
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={4}>
+        {/* Github */}
+        <Link href='https://github.com/trevv16'>
+          <img
+            className={classes.social_icons}
+            src='/github_icon.png'
+            alt='github icon'
+            onMouseOver={console.log('test')}
+          />
+        </Link>
+      </Grid>
+      <Grid item xs={4}>
+        {/* LinkedIn */}
+        <Link href='https://linkedin.com/in/trevornjeru'>
+          <img
+            className={classes.social_icons}
+            src='/linkedin_icon.png'
+            alt='linkedin icon'
+          />
+        </Link>
+      </Grid>
+      <Grid item xs={4}>
+        {/* Instagram */}
+        <Link href='https://instagram.com/iamtrevvn'>
+          <img
+            className={classes.social_icons}
+            src='/instagram_icon.png'
+            alt='instagram icon'
+          />
+        </Link>
+      </Grid>
+    </Grid>
+  );
+}
 
 function Footer() {
   const classes = useStyles();
@@ -37,7 +92,14 @@ function Footer() {
     <div>
       <CssBaseline />
       <footer className={classes.root}>
-        <Copyright />
+        <Grid container spacing={3}>
+          <Grid item xs={12} className={classes.icon_group}>
+            <Icons />
+          </Grid>
+          <Grid item xs={12}>
+            <Copyright />
+          </Grid>
+        </Grid>
       </footer>
     </div>
   );
