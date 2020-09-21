36fabae72c84b26c380d634b9d1904dd86c30572
Trevor Njeru <tnjeru16@gmail.com>
2020-09-21 02:26:43 -0400

    added component import

--- src/frontend/src/components/index.js
+++ src/frontend/src/components/index.js
@@ -1,7 +1,9 @@
 export { default as Introduction } from '../components/_home_page/intro/Introduction';
 export { default as Jumbotron } from '../components/_home_page/jumbotron/Jumbotron';
 export { default as MainSkills } from '../components/_home_page/main_skills/MainSkills';
-export { default as RecentProjects } from '../components/_home_page/recent_projects/RecentProjects';
+export { default as ProjectCard } from '../components/project/Project';
+export { ProjectHighlight } from '../components/project/Project';
+export { ProjectDetail } from '../components/project/Project';
 
 export { default as CareerStats } from '../components/_resume_page/career_stats/CareerStats';
 export { default as Education } from '../components/_resume_page/education_history/EducationHistory';
