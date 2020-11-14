2ad0ebe458cba8580e0f363e52c0ad8326eebe75
Trevor Njeru <tnjeru16@gmail.com>
2020-11-14 17:25:58 -0500

    converted project view comp to func comp w/ hooks

--- src/frontend/src/views/projects/Project.js
+++ src/frontend/src/views/projects/Project.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import { CssBaseline, Grid, makeStyles, Typography } from '@material-ui/core';
 import _ from 'underscore';
@@ -10,28 +10,18 @@ import {
   ProjectCard
 } from '../../components/index';
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     flexGrow: 1
-//   },
-//   main: {
-//     marginTop: theme.spacing(18),
-//     marginBottom: theme.spacing(2)
-//   }
-// }));
-
-export default class Project extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      projects: []
-    };
-
-    this.fetchProjects = this.fetchProjects.bind(this);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  main: {
+    marginTop: theme.spacing(18),
+    marginBottom: theme.spacing(2)
   }
+}));
 
-  fetchProjects = () => {
+export default function Project(props) {
+  const fetchProjects = () => {
     return Promise.resolve(
       api
         .fetch(`v1/projects`)
@@ -43,93 +33,47 @@ export default class Project extends Component {
         })
     );
   };
+  const classes = useStyles();
+  const [projects, handleProjects] = useState([]);
+  // console.log('Projects: ', projectData);
 
-  componentDidMount() {
-    this.fetchProjects()
+  useEffect(() => {
+    fetchProjects()
       .then((data) => {
-        this.setState({
-          projects: [...data]
-        });
+        handleProjects([...data]);
       })
       .catch((err) => {
         console.log(err);
       });
-  }
+  }, []);
 
-  render() {
-    const classes = this.props;
-    // console.log('Projects: ', this.state.projects);
-    return (
-      <div className={classes.root}>
-        <Helmet>
-          <meta charSet='utf-8' />
-          <title>Projects | Trevor's Portfolio</title>
-        </Helmet>
-        <CssBaseline />
-        <Navigation />
-        <Grid container spacing={1} className={classes.main}>
-          <Grid item xs={12}>
-            <Typography align='center' variant='h1'>
-              Projects
-            </Typography>
-          </Grid>
-          <Grid container spacing={4} className={classes.main}>
-            {this.state.projects.map((proj, i) => {
-              console.log('proj', proj);
-              return (
-                <Grid item xs={6} key={i}>
-                  <ProjectCard project={proj} />
-                </Grid>
-              );
-            })}
-          </Grid>
+  return (
+    <div className={classes.root}>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Projects | Trevor's Portfolio</title>
+      </Helmet>
+      <CssBaseline />
+      <Navigation />
+      <Grid container spacing={1} className={classes.main}>
+        <Grid item xs={12}>
+          <Typography align='center' variant='h1'>
+            Projects
+          </Typography>
         </Grid>
-        <MailingList />
-        <Footer />
-      </div>
-    );
-  }
+        <Grid container spacing={4} className={classes.main}>
+          {projects.map((proj, i) => {
+            console.log('proj', proj);
+            return (
+              <Grid item xs={6} key={i}>
+                <ProjectCard project={proj} />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Grid>
+      <MailingList />
+      <Footer />
+    </div>
+  );
 }
-
-// function Project(props) {
-//   const classes = useStyles();
-//   // let projectData = [];
-//   const projectData = fetchProjects()
-//     .then((data) => {
-//       return data;
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-
-//   return (
-//     <div className={classes.root}>
-//       <Helmet>
-//         <meta charSet='utf-8' />
-//         <title>Projects | Trevor's Portfolio</title>
-//       </Helmet>
-//       <CssBaseline />
-//       <Navigation />
-//       <Grid container spacing={1} className={classes.main}>
-//         <Grid item xs={12}>
-//           <Typography align='center' variant='h1'>
-//             Projects
-//           </Typography>
-//         </Grid>
-//         <Grid container spacing={4} className={classes.main}>
-//           {projectData.map((proj, i) => {
-//             return (
-//               <Grid item xs={6}>
-//                 <ProjectCard key={i} project={proj} />,
-//               </Grid>
-//             );
-//           })}
-//         </Grid>
-//       </Grid>
-//       <MailingList />
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default Project;
