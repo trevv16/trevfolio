5a4418695c58280107332a57bdfe8c1cf0ca4602
trevv16 <tnjeru16@gmail.com>
2020-11-21 12:53:08 -0500

    detailed all upcoming routes

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -22,8 +22,54 @@ function App() {
           <Route exact path='/resume' component={Resume} />
           <Route exact path='/contact' component={Contact} />
           <Route exact path='/projects' component={Project} />
-          <Route path='/skill' component={SkillList} />
-          <Route path='/skill?' component={Skill} />
+          {/* <Route exact path='/projects/projectID' component={Project} />
+          <Route exact path='/projects?skill=' component={Project} />
+          <Route path='/skills' component={SkillList} /> */}
+
+          {/* <Route exact path='/signup' component={Project} />
+          <Route exact path='/signin' component={Project} />
+          <Route exact path='/forgot' component={Project} />
+          <Route exact path='/reset' component={Project} /> */}
+
+          {/* <Route exact path='/blogs' component={Project} />
+          <Route exact path='/blogs?' component={Project} />
+          <Route exact path='/blogs/blogID' component={Project} />
+          <Route exact path='/blogs/blogID/posts' component={Project} />
+          <Route exact path='/blogs/blogID/posts/postID' component={Project} /> */}
+
+          {/* ADMIN ROUTES */}
+          {/* <Route exact path='/admin' component={Project} />
+          <Route exact path='/admin/settings' component={Project} /> */}
+
+          {/* <Route exact path='/admin/blogs' component={Project} />
+          <Route exact path='/admin/blogs/blogID' component={Project} />
+          <Route exact path='/admin/blogs/blogID/edit' component={Project} />
+          <Route exact path='/admin/blogs/blogID/posts' component={Project} />
+          <Route exact path='/admin/blogs/blogID/posts/postID' component={Project} />
+          <Route exact path='/admin/blogs/blogID/posts/postID/edit' component={Project} /> */}
+
+          {/* <Route exact path='/admin/projects' component={Project} />
+          <Route exact path='/admin/projects/projectID' component={Project} />
+          <Route exact path='/admin/projects/projectID/edit' component={Project} /> */}
+
+          {/* <Route exact path='/admin/mailing_lists' component={Project} />
+          <Route exact path='/admin/mailing_lists/listID' component={Project} />
+          <Route exact path='/admin/mailing_lists/listID/sent' component={Project} />
+          <Route exact path='/admin/mailing_lists/listID/sent/sentID' component={Project} />
+          <Route exact path='/admin/mailing_lists/listID/drafts' component={Project} />
+          <Route exact path='/admin/mailing_lists/listID/drafts/draftID' component={Project} /> */}
+
+          {/* <Route exact path='/admin/messages' component={Project} />
+          <Route exact path='/admin/messages/messageID' component={Project} />
+          <Route exact path='/admin/messages/messageID/draft' component={Project} /> */}
+
+          {/* <Route exact path='/admin/skills' component={Project} />
+          <Route exact path='/admin/skills/skillID' component={Project} />
+          <Route exact path='/admin/skills/skillID/edit' component={Project} /> */}
+
+          {/* <Route exact path='/admin/pages' component={Project} />
+          <Route exact path='/admin/pages/pageID' component={Project} />
+          <Route exact path='/admin/pages/pageID/edit' component={Project} /> */}
         </Switch>
       </Router>
     </div>
