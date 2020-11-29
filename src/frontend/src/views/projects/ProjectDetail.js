119750d392d21a4c05b28bb732b29df92d328837
trevv16 <tnjeru16@gmail.com>
2020-11-29 15:45:00 -0500

    added conditional render and gallery comp

--- src/frontend/src/views/projects/ProjectDetail.js
+++ src/frontend/src/views/projects/ProjectDetail.js
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 import { CssBaseline, Grid, makeStyles, Typography } from '@material-ui/core';
 import _ from 'underscore';
 import api from '../../utils/api';
-import { MailingList, Navigation, Footer } from '../../components/index';
+import {
+  MailingList,
+  Navigation,
+  Footer,
+  Gallery
+} from '../../components/index';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1
   },
   main: {
-    marginTop: theme.spacing(18),
+    marginTop: theme.spacing(0),
     marginBottom: theme.spacing(2)
   }
 }));
@@ -30,12 +35,32 @@ export default function ProjectDetail(props) {
     );
   };
   const classes = useStyles();
-  const [projects, handleProjects] = useState([]);
+  const [project, handleProject] = useState([]);
+  const imgList = [
+    {
+      name: 'image1',
+      src: 'https://source.unsplash.com/random/1600x600',
+      description: 'Trev'
+    },
+    {
+      name: 'image2',
+      src: 'https://source.unsplash.com/random/1600x600',
+      description: 'Trev'
+    },
+    {
+      name: 'image2',
+      src: 'https://source.unsplash.com/random/1600x600',
+      description: 'Trev'
+    }
+  ];
 
+  const dataReady = project.title !== undefined ? true : false;
+
+  console.log('proj: ', project);
   useEffect(() => {
     fetchProjects()
       .then((data) => {
-        handleProjects([...data]);
+        handleProject([...data]);
       })
       .catch((err) => {
         console.log(err);
@@ -50,13 +75,23 @@ export default function ProjectDetail(props) {
       </Helmet>
       <CssBaseline />
       <Navigation />
-      <Grid container spacing={1} className={classes.main}>
-        <Grid item xs={12}>
-          <Typography align='center' variant='h1'>
-            Projects
-          </Typography>
+      {project && dataReady && (
+        <Grid container spacing={1} className={classes.main}>
+          <Grid item xs={12}>
+            <Gallery
+              animation={'slide'}
+              interval={5500}
+              timeout={800}
+              images={imgList}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography align='center' variant='h2'>
+              {`${project.title}`}
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
+      )}
       <MailingList />
       <Footer />
     </div>
