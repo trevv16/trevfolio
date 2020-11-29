898acdb9758e4aa375fd58da527d02341459b059
trevv16 <tnjeru16@gmail.com>
2020-11-29 15:45:25 -0500

    fixed list sort method, render data from props

--- src/frontend/src/components/project/ProjectDetailDrawer.js
+++ src/frontend/src/components/project/ProjectDetailDrawer.js
@@ -34,34 +34,38 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function ProjectDetailDrawer(props) {
-    const sortedList = props.processList ? props.processList : null;
   const classes = useStyles();
+
+  const sortedList = () => {
+    if (props.processList) {
+      return props.processList.sort((a, b) => (a.order > b.order ? 1 : -1));
+    } else {
+      return null;
+    }
+  };
 
   return (
     <div className={classes.root}>
-        {sortedList && 
-        sortedList.map((item) => {
-            return (
-                <Accordion
-                    square
-                    expanded={expanded === 'panel1'}
-                    onChange={handleChange('panel1')}
-                >
-                    <AccordionSummary aria-controls='panel1d-content' id='panel1d-header'>
-                        <Typography>{`${item.}`}</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                    <Typography>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                        malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
-                        dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-                        lacus ex, sit amet blandit leo lobortis eget.
-                    </Typography>
-                    </AccordionDetails>
-                </Accordion>
-            );
-        });
-        }
+      {sortedList &&
+        sortedList.map((process) => {
+          return (
+            <Accordion
+              square
+              expanded={expanded === 'panel1'}
+              onChange={handleChange('panel1')}
+            >
+              <AccordionSummary
+                aria-controls='panel1d-content'
+                id='panel1d-header'
+              >
+                <Typography>{`${process.type}`}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>{`${process.content}`}</Typography>
+              </AccordionDetails>
+            </Accordion>
+          );
+        })}
     </div>
   );
 }
