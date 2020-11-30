663b05b42be18cd439a1450ee0a5d16cdaf51a19
trevv16 <tnjeru16@gmail.com>
2020-11-30 12:11:39 -0500

    debugged render errors for proj detail drawer

--- src/frontend/src/components/project/ProjectDetailDrawer.js
+++ src/frontend/src/components/project/ProjectDetailDrawer.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import {
-  Link,
-  GridList,
-  GridListTile,
-  GridListTileBar,
-  IconButton,
-  makeStyles,
-  MuiAccordion,
-  MuiAccordionSummary,
-  MuiAccordionDetails,
-  Typography
-} from '@material-ui/core';
-import InfoIcon from '@material-ui/icons/Info';
+import React, { useEffect, useState } from 'react';
+import { makeStyles, Typography, withStyles, Grid } from '@material-ui/core';
+import MuiAccordion from '@material-ui/core/Accordion';
+import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
+import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,42 +21,105 @@ const useStyles = makeStyles((theme) => ({
   },
   tile: {
     minHeight: '450px'
+  },
+  drawer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: theme.spacing(10)
   }
 }));
 
+const Accordion = withStyles({
+  root: {
+    border: '1px solid rgba(0, 0, 0, .125)',
+    boxShadow: 'none',
+    '&:not(:last-child)': {
+      borderBottom: 0
+    },
+    '&:before': {
+      display: 'none'
+    },
+    '&$expanded': {
+      margin: 'auto'
+    }
+  },
+  expanded: {}
+})(MuiAccordion);
+
+const AccordionSummary = withStyles({
+  root: {
+    backgroundColor: 'rgba(0, 0, 0, .03)',
+    borderBottom: '1px solid rgba(0, 0, 0, .125)',
+    marginBottom: -1,
+    minHeight: 56,
+    '&$expanded': {
+      minHeight: 56
+    }
+  },
+  content: {
+    '&$expanded': {
+      margin: '12px 0'
+    }
+  },
+  expanded: {}
+})(MuiAccordionSummary);
+
+const AccordionDetails = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2)
+  }
+}))(MuiAccordionDetails);
+
 export default function ProjectDetailDrawer(props) {
+  const [expanded, setExpanded] = React.useState('panel1');
+  const [sortedList, updateProcessList] = React.useState([]);
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
   const classes = useStyles();
 
-  const sortedList = () => {
-    if (props.processList) {
-      return props.processList.sort((a, b) => (a.order > b.order ? 1 : -1));
+  const sortList = () => {
+    if (props.process !== undefined) {
+      const sorted = props.process.sort((a, b) => (a.order > b.order ? 1 : -1));
+      console.log(sorted);
+      return sorted;
     } else {
       return null;
     }
   };
 
+  useEffect(() => {
+    let sortedProcessList = sortList();
+    updateProcessList(sortedProcessList);
+  }, []);
+
   return (
     <div className={classes.root}>
-      {sortedList &&
-        sortedList.map((process) => {
-          return (
-            <Accordion
-              square
-              expanded={expanded === 'panel1'}
-              onChange={handleChange('panel1')}
-            >
-              <AccordionSummary
-                aria-controls='panel1d-content'
-                id='panel1d-header'
-              >
-                <Typography>{`${process.type}`}</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>{`${process.content}`}</Typography>
-              </AccordionDetails>
-            </Accordion>
-          );
-        })}
+      <Grid container spacing={0} className={classes.drawer}>
+        {Array.isArray(sortedList) &&
+          sortedList.map((process, i) => {
+            return (
+              <Grid item xs={8} key={i}>
+                <Accordion
+                  square
+                  expanded={expanded === `panel${i + 1}`}
+                  onChange={handleChange(`panel${i + 1}`)}
+                >
+                  <AccordionSummary
+                    aria-controls={`panel${i + 1}d-content`}
+                    id={`panel${i + 1}d-header`}
+                  >
+                    <Typography>{`${process.type}`}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <Typography>{`${process.content}`}</Typography>
+                  </AccordionDetails>
+                </Accordion>
+              </Grid>
+            );
+          })}
+      </Grid>
     </div>
   );
 }
