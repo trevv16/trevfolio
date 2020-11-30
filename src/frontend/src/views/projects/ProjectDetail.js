7ae75799262a500584fd31bc518cc506057b18ee
trevv16 <tnjeru16@gmail.com>
2020-11-30 12:11:19 -0500

    fixed render errors, displays project data.

--- src/frontend/src/views/projects/ProjectDetail.js
+++ src/frontend/src/views/projects/ProjectDetail.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
-import { CssBaseline, Grid, makeStyles, Typography } from '@material-ui/core';
-import _ from 'underscore';
+import {
+  CssBaseline,
+  Grid,
+  makeStyles,
+  Typography,
+  CircularProgress
+} from '@material-ui/core';
 import api from '../../utils/api';
 import {
   MailingList,
   Navigation,
   Footer,
-  Gallery
+  Gallery,
+  ProjectDetailDrawer
 } from '../../components/index';
 
 const useStyles = makeStyles((theme) => ({
@@ -17,25 +23,21 @@ const useStyles = makeStyles((theme) => ({
   main: {
     marginTop: theme.spacing(0),
     marginBottom: theme.spacing(2)
+  },
+  loading: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: theme.spacing(10),
+    marginBottom: theme.spacing(12)
   }
 }));
 
 export default function ProjectDetail(props) {
+  const [isBusy, setBusy] = useState(true);
   const _id = props.match.params.id;
-  const fetchProjects = () => {
-    return Promise.resolve(
-      api
-        .fetch(`v1/projects/${_id}`)
-        .then((response) => {
-          return _.first(response.data, 4);
-        })
-        .catch((err) => {
-          console.log(err);
-        })
-    );
-  };
   const classes = useStyles();
-  const [project, handleProject] = useState([]);
+  const [project, handleProject] = useState(null);
   const imgList = [
     {
       name: 'image1',
@@ -53,19 +55,96 @@ export default function ProjectDetail(props) {
       description: 'Trev'
     }
   ];
+  const hasGallery =
+    project === null || project.gallery.length === 0 ? false : true;
 
-  const dataReady = project.title !== undefined ? true : false;
+  const fetchProjects = () => {
+    return Promise.resolve(
+      api
+        .fetch(`v1/projects/${_id}`)
+        .then((response) => {
+          return response.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    );
+  };
 
-  console.log('proj: ', project);
   useEffect(() => {
     fetchProjects()
       .then((data) => {
-        handleProject([...data]);
+        if (data !== [] || data[0] !== undefined) {
+          setBusy(false);
+          handleProject(data[0]);
+        } else {
+          setBusy(true);
+        }
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+
+  const DetailData = () => {
+    return (
+      <React.Fragment>
+        {project && (
+          <Grid container spacing={1} className={classes.main}>
+            <Grid item xs={12}>
+              <Gallery
+                animation={'slide'}
+                interval={5500}
+                timeout={800}
+                images={imgList}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography align='center' variant='h2'>
+                {`${project.title}`}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography align='center' variant='body1'>
+                {`${project.description}`}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography align='center' variant='h5'>
+                {`Demo: ${project.demo_url}`}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography align='center' variant='h5'>
+                {`Github: ${project.github_url}`}
+              </Typography>
+            </Grid>
+            {hasGallery && (
+              <Grid item xs={12}>
+                <Gallery
+                  animation={'slide'}
+                  interval={5500}
+                  timeout={800}
+                  images={project.gallery}
+                />
+              </Grid>
+            )}
+            <Grid item xs={12}>
+              <ProjectDetailDrawer process={project.process} />
+            </Grid>
+          </Grid>
+        )}
+      </React.Fragment>
+    );
+  };
+
+  const Loading = () => {
+    return (
+      <React.Fragment>
+        <CircularProgress color='secondary' className={classes.loading} />
+      </React.Fragment>
+    );
+  };
 
   return (
     <div className={classes.root}>
@@ -75,23 +154,7 @@ export default function ProjectDetail(props) {
       </Helmet>
       <CssBaseline />
       <Navigation />
-      {project && dataReady && (
-        <Grid container spacing={1} className={classes.main}>
-          <Grid item xs={12}>
-            <Gallery
-              animation={'slide'}
-              interval={5500}
-              timeout={800}
-              images={imgList}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Typography align='center' variant='h2'>
-              {`${project.title}`}
-            </Typography>
-          </Grid>
-        </Grid>
-      )}
+      {isBusy ? <Loading /> : <DetailData />}
       <MailingList />
       <Footer />
     </div>
