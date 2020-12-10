b71a4f39bd3cc3e4acf67d954aa0103afbcc79be
trevv16 <tnjeru16@gmail.com>
2020-12-09 20:03:42 -0500

    removed unused components

--- src/frontend/src/views/index.js
+++ src/frontend/src/views/index.js
@@ -2,7 +2,5 @@ export { default as Home } from '../views/home/Home';
 export { default as About } from '../views/about/About';
 export { default as Resume } from '../views/resume/Resume';
 export { default as Contact } from '../views/contact/Contact';
-export { default as Skill } from '../components/skills/Skill';
-export { default as SkillList } from '../components/skills/Skill';
 export { default as Project } from '../views/projects/Project';
 export { default as ProjectDetail } from '../views/projects/ProjectDetail';
