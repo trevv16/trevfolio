26e4c07d3113322b6ca3d2614b5c4728ece8fecb
trevv16 <tnjeru16@gmail.com>
2021-01-21 23:13:10 -0500

    added cors support

--- src/backend/app.js
+++ src/backend/app.js
@@ -9,6 +9,7 @@ const flash = require('express-flash');
 const session = require('express-session');
 const passport = require('passport');
 const methodOverride = require('method-override');
+const cors = require('cors');
 
 // Configs
 const mongoConfig = require('./config/db_config');
@@ -48,6 +49,7 @@ passConfig.initializePassport(
   (id) => dbService.getUserById(id)
 );
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -68,7 +70,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride('_method'));
 app.use(cookieParser());
-app.use(express.static('src/frontend/build'));
+app.use(express.static(path.join(__dirname, 'src/frontend/build')));
 
 // Set Routes
 app.use('/auth', authRouter);
