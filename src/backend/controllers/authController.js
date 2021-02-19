1e8fac15768fc4418a9306615f1e1c9e72bbcc55
trevv16 <tnjeru16@gmail.com>
2021-02-19 18:12:22 -0500

    fixed sign up and sign in routes

--- src/backend/controllers/authController.js
+++ src/backend/controllers/authController.js
@@ -1,24 +1,73 @@
-const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const User = require('../models/user');
 
 module.exports = {
-  signUp: (req, res, next) => {
-    res.send("Sign Up");
+  signUp: async (req, res, next) => {
+    const {first_name, last_name, email, password} = req.body;
+    const _id = mongoose.Types.ObjectId();
+    try {
+      const genUser = await User.create({_id, first_name, last_name, email, password});
+
+      res.status(201).json({
+        success: true,
+        user: genUser
+      })
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        error: err.message
+      })
+    }
   },
-  signIn: (req, res, next) => {
-    res.send("Sign In");
+  signIn: async (req, res, next) => {
+    const {email, password} = req.body;
+
+    if(!email || !password) {
+      res.status(400).json({
+        success: false,
+        error: "Please provide an email and password"
+      });
+    }
+
+    try {
+      const user = await User.findOne({email}).select("+password");
+
+      if(!user) {
+        res.status(404).json({
+          success: false,
+          error: "Invalid credentials"
+        });
+      }
+
+      const isMatch = await user.comparePasswords(password);
+
+      if(!isMatch) {
+        res.status(404).json({
+          success: false,
+          error: "Invalid credentials"
+        });
+      }
+
+      res.status(201).json({
+        success: true,
+        token: "afdaklb3nl243trwl4en",
+        user: user
+      })
+
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        error: err.message
+      });
+    }
   },
-  signOut: (req, res, next) => {
+  signOut: async (req, res, next) => {
     res.send("Sign Out");
   },
-  forgot: (req, res, next) => {
+  forgot: async (req, res, next) => {
     res.send("Forgot");
   },
-  reset: (req, res, next) => {
+  reset: async (req, res, next) => {
     res.send("Reset");
   }
 };
-
-async function hashing(pwd) {
-  const salt = await bcrypt.genSalt(10);
-  return await bcrypt.hash(pwd, salt);
-}
