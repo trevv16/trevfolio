802749e44b711b31022cb69b0b3ad3926c3ab9f3
trevv16 <tnjeru16@gmail.com>
2021-02-19 18:06:00 -0500

    cleaned up user schema methods, virtuals, etc

--- src/backend/models/user.js
+++ src/backend/models/user.js
@@ -51,103 +51,27 @@ const userSchema = new mongoose.Schema({
   ]
 });
 
-userSchema.plugin(timestamps);
-
-const User = mongoose.model('User', userSchema, 'users');
-
-/**
- ******************* Defined Functions
- */
-
-/**
- *
- * @param {String} email raw value in MongoDB `email`
- * @return {String} privateEmail format -- '**@gmail.com'
- */
-async function obfuscate(email) {
-  const separatorIndex = email.indexOf('@');
-  if (separatorIndex < 3) {
-    // 'ab@gmail.com' -> '**@gmail.com'
-    return (
-      email.slice(0, separatorIndex).replace(/./g, '*') +
-      email.slice(separatorIndex)
-    );
+userSchema.pre("save", async function(next) {
+  if(!this.isModified("password")) {
+    next();
   }
-  // 'test42@gmail.com' -> 'te****@gmail.com'
-  return (
-    email.slice(0, 2) +
-    email.slice(2, separatorIndex).replace(/./g, '*') +
-    email.slice(separatorIndex)
-  );
+
+  this.password = await hashing(this.password);
+  next();
+});
+
+async function hashing(pwd) {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(pwd, salt);
 }
 
-/**
- ******************* Virtuals
- */
+userSchema.methods.comparePasswords = async function(pwd) {
+  return await bcrypt.compare(pwd, this.password);
+}
 
-/**
- * Virtual for user's full name
- */
-userSchema
-  .virtual('name')
-  .get(function () {
-    // To avoid errors in cases where an author does not have either a last name or first name
-    // We want to make sure we handle the exception by returning an empty string for that case
-    return this.first_name && this.last_name
-      ? `${this.first_name} ${this.last_name}`
-      : '';
-  })
-  .set(function (v) {
-    // `v` is the value being set, so use the value to set
-    // `firstName` and `lastName`.
-    const first_name = v.substring(0, v.indexOf(' '));
-    const last_name = v.substring(v.indexOf(' ') + 1);
-    this.set({ first_name, last_name });
-  });
+userSchema.plugin(timestamps);
 
-/**
- ******************* Methods
- */
-userSchema.methods = {
-  /**
-   * Called when checking if user input matches
-   * their current saved password
-   * @param  {String} inputPassword
-   */
-  checkPassword: async (inputPassword) => {
-    return bcrypt.compareSync(inputPassword, this.password);
-  },
-  /**
-   * Called when first saving users password to the db
-   * @param  {String} plainTextPassword
-   */
-  hashPassword: async (plainTextPassword) => {
-    return bcrypt.hashSync(plainTextPassword, 10);
-  },
-  /**
-   * @returns  {String} email from MongoDB
-   */
-  privateEmail: async () => {
-    return obfuscate(this.email);
-  }
-};
 
-/**
- ******************* Hooks
- */
-
-/**
- *
- * @param  {function} next
- */
-userSchema.pre('save', (next) => {
-  if (!this.password) {
-    console.error('User: No password provided');
-    next();
-  } else {
-    this.password = this.hashPassword(this.password);
-    next();
-  }
-});
+const User = mongoose.model('User', userSchema, 'users');
 
 module.exports = User;
