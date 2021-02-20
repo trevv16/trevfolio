5a403dbc2116c9344c10a365f8214fce54dd6569
trevv16 <tnjeru16@gmail.com>
2021-02-19 19:14:23 -0500

    eslint styling

--- src/backend/controllers/authController.js
+++ src/backend/controllers/authController.js
@@ -4,16 +4,22 @@ const ErrorResponse = require('../utils/errorResponse');
 
 module.exports = {
   signUp: async (req, res, next) => {
-    const {first_name, last_name, email, password} = req.body;
+    const { first_name, last_name, email, password } = req.body;
     const _id = mongoose.Types.ObjectId();
     try {
-      const findUser = await User.findOne({email}).select("+password");
+      const findUser = await User.findOne({ email }).select('+password');
 
-      if(findUser) {
-        return next(new ErrorResponse("User already exists", 401));
+      if (findUser) {
+        return next(new ErrorResponse('User already exists', 401));
       }
 
-      const user = await User.create({_id, first_name, last_name, email, password});
+      const user = await User.create({
+        _id,
+        first_name,
+        last_name,
+        email,
+        password
+      });
 
       sendToken(user, 201, res);
     } catch (err) {
@@ -21,23 +27,25 @@ module.exports = {
     }
   },
   signIn: async (req, res, next) => {
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
-    if(!email || !password) {
-      return next(new ErrorResponse("Please provide an email and password", 400));
+    if (!email || !password) {
+      return next(
+        new ErrorResponse('Please provide an email and password', 400)
+      );
     }
 
     try {
-      const user = await User.findOne({email}).select("+password");
+      const user = await User.findOne({ email }).select('+password');
 
-      if(!user) {
-        return next(new ErrorResponse("Invalid Credentials", 401));
+      if (!user) {
+        return next(new ErrorResponse('Invalid Credentials', 401));
       }
 
       const isMatch = await user.comparePasswords(password);
 
-      if(!isMatch) {
-        return next(new ErrorResponse("Invalid Credentials", 401));
+      if (!isMatch) {
+        return next(new ErrorResponse('Invalid Credentials', 401));
       }
       sendToken(user, 200, res);
     } catch (err) {
@@ -45,18 +53,17 @@ module.exports = {
     }
   },
   signOut: async (req, res, next) => {
-    res.send("Sign Out");
+    res.send('Sign Out');
   },
   forgot: async (req, res, next) => {
-    res.send("Forgot");
+    const { email } = req.body;
   },
   reset: async (req, res, next) => {
-    res.send("Reset");
+    res.send('Reset');
   }
 };
 
-
 const sendToken = (user, statusCode, res) => {
   const token = user.getSignedToken();
-  res.status(statusCode).json({success: true, token});
+  res.status(statusCode).json({ success: true, token });
 };
