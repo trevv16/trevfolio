716d533aa0f629aa1793c0c76632b8840de5b660
trevv16 <tnjeru16@gmail.com>
2021-02-23 17:12:34 -0500

    added query by id for message content

--- src/backend/frontend/src/views/admin/messages/AdminMessageDetailPage.js
+++ src/backend/frontend/src/views/admin/messages/AdminMessageDetailPage.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Grid, Typography, makeStyles } from '@material-ui/core';
 import { AdminNavigation, AdminFooter } from '../../../components/index';
+import api from '../../../utils/api';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1
+  },
+  center: {
+    justifyContent: 'center'
   },
   main: {
     justifyContent: 'center',
@@ -14,20 +18,56 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-function AdminMessageDetailPage() {
+function AdminMessageDetailPage(props) {
+  const [message, handleMessage] = useState({});
   const classes = useStyles();
+
+  useEffect(() => {
+    api
+      .fetch(`v1/inquiries/${props.match.params.messageID}`)
+      .then((response) => {
+        if (response && response.data !== []) {
+          console.log(response);
+          let msgData = response.data[0];
+          let created = msgData.createdAt ? msgData.createdAt : 'Unknown';
+          let name = msgData.first_name + ' ' + msgData.last_name;
+
+          let msg = {
+            name,
+            email: msgData.email,
+            msg: msgData.message,
+            created
+          };
+
+          handleMessage(msg);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
 
   return (
     <div className={classes.root}>
       <Helmet>
         <meta charSet='utf-8' />
-        <title>Settings | Portfolio Admin</title>
+        <title>{`Message from ${message.name} | Portfolio Admin`}</title>
       </Helmet>
       <AdminNavigation />
       <Grid container spacing={3} className={classes.main}>
         <Grid item xs={10}>
-          <Typography align='center' variant='h1' component='h1'>
-            Dashboard
+          <Typography align='center' variant='h3' component='h1'>
+            Message from,{message.name}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography align='center' variant='h5' component='h5'>
+            {message.email}
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography align='center' variant='body1' className={classes.msg}>
+            {message.msg}
           </Typography>
         </Grid>
       </Grid>
